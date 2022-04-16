e9629bce5bf8760746ed231b33b7de056a6dba87
Nam <nampro1210@gmail.com>
2022-04-16 22:08:29 +0700

    Initial commit

---
+++ src/main/java/com/example/server_capstone/ServerCapstoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.server_capstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerCapstoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerCapstoneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/server_capstone/ServerCapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.server_capstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerCapstoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
